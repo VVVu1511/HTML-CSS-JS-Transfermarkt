41df665e004c112591684d25200f8aba22360122
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-01-16 09:32:05 +0700

    add 2 features to see new infor, add some images

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 //set border for daddy search box
 
+function changeContentOfMostViewedPlayer(){
+    left = document.querySelector('.spotlight-section-lists-advertise--advertiseviewed---view .left-right .left');
+    right = document.querySelector('.spotlight-section-lists-advertise--advertiseviewed---view .left-right .right');
+    inforOfViewedSection = document.querySelectorAll('.spotlight-section-lists-advertise--advertiseviewed---view .infor');
+
+    let active = 0;
+    let preactive = 0;
+
+    left.addEventListener('click',()=>{
+        preactive = active;
+        if(active > 0) active = active - 1;
+        
+        inforOfViewedSection[active].style.display = 'flex';
+        inforOfViewedSection[preactive].style.display = 'none';
+    });
+
+    right.addEventListener('click',()=>{
+        preactive = active;
+        if(active < inforOfViewedSection.length - 1) active = active + 1;
+    
+        inforOfViewedSection[active].style.display = 'flex';
+        inforOfViewedSection[preactive].style.display = 'none';
+    });
+
+}
+
 function setBlueBorderForHeaderSearch(){
     let text = document.querySelector('.content-header-searching-search-text');
     let search_box = document.querySelector('.content-header-searching-search');
@@ -22,6 +48,13 @@ function setBlueBorderForHeaderSearch(){
     })
 }
 
+function supportChangingContent(listOfDots,listOfSpotlightSection,preactive,active){
+    listOfSpotlightSection[preactive].style.display = "none";
+    listOfDots[preactive].style.color = "white";
+    listOfSpotlightSection[active].style.display = "block";
+    listOfDots[active].style.color = "gray";
+}
+
 function changeContentOfSpotlight(){
     let listOfDots = document.querySelectorAll('.spotlight-section-lists-advertise--list-dots .fa-solid');
     let listOfLeftArrows = document.querySelectorAll('.fa-angle-left');
@@ -38,33 +71,35 @@ function changeContentOfSpotlight(){
         }
     }
 
-    preactive = active;
-
-
     for(let i = 0; i < listOfLeftArrows.length; i++){
         listOfLeftArrows[i].addEventListener('click',()=>{
+            preactive = active;
             if(active > 0) active = active - 1;
+            supportChangingContent(listOfDots,listOfSpotlightSection,preactive,active);
         });
     }
 
     for(let j = 0; j < listOfRightArrows.length; j++){
         listOfRightArrows[j].addEventListener('click',()=>{
+            preactive = active;
             if(active < listOfDots.length - 1) active = active + 1;
+            supportChangingContent(listOfDots,listOfSpotlightSection,preactive,active);
         });
     }
 
-    if(preactive != -1){
-        listOfSpotlightSection[preactive].style.display = 'none';
-        listOfDots[preactive].style.color = 'white';
+    for(let j = 0; j < listOfDots.length; j++){
+        listOfDots[j].addEventListener('click',()=>{
+            preactive = active;
+            active = j;
+            supportChangingContent(listOfDots,listOfSpotlightSection,preactive,active);
+        })
     }
 
-    if(active != -1){
-        listOfSpotlightSection[active].style.display = 'block';
-        listOfDots[active].style.color = 'gray';
-    }
 }
 
 setBlueBorderForHeaderSearch();
 changeContentOfSpotlight();
+changeContentOfMostViewedPlayer();
+
 
 
